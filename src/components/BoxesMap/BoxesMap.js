80bd82d9df8c39ae21299f82d52a07ec42c35656
Morgane Broutet <morgane.broutet@gmail.com>
2021-01-18 00:01:52 +0100

    add data source & map container

---
+++ src/components/BoxesMap/BoxesMap.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./BoxesMap.css";
+
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
+
+const DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const BoxesMap = () => {
+  const position = [48.856614, 2.3522219];
+
+  return (
+    <MapContainer
+      className="map-container"
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          Adresse <br /> Pays
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default BoxesMap;
