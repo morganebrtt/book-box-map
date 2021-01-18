1ef4656f2bae5fd4a601215db055917e1d68852f
Morgane Broutet <morgane.broutet@gmail.com>
2021-01-18 16:34:50 +0100

    add markers on map

--- src/components/BoxesMap/BoxesMap.js
+++ src/components/BoxesMap/BoxesMap.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { readRemoteFile } from "react-papaparse";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -15,7 +16,25 @@ const DefaultIcon = L.icon({
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const BoxesMap = () => {
+  const [boxesData, setBoxesData] = useState([]);
+  const [hasData, setHasData] = useState(false);
   const position = [48.856614, 2.3522219];
+
+  if (!hasData) {
+    setHasData(true);
+    readRemoteFile("./data/boite_a_lire.csv", {
+      header: true,
+      comments: "//",
+      transformHeader: (columnName) => columnName.trim(),
+      complete: (results) => {
+        setBoxesData(results.data);
+      },
+      error: (error) => {
+        console.error("error:", error);
+      },
+      skipEmptyLines: "greedy",
+    });
+  }
 
   return (
     <MapContainer
@@ -28,11 +47,26 @@ const BoxesMap = () => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          Adresse <br /> Pays
-        </Popup>
-      </Marker>
+      <div>
+        {boxesData.length > 0 &&
+          boxesData.map((boxData, index) => {
+            const boxPositionGPS = boxData.Coord_GPS;
+            const boxPositionStr = boxPositionGPS.split(",");
+            const boxPosition = boxPositionStr.map((i) => Number(i));
+            return (
+              // eslint-disable-next-line react/no-array-index-key
+              <Marker key={index} position={boxPosition}>
+                <Popup>
+                  <p>{boxData.Adresse}</p>
+                  <p>{boxData.Code_Postal}</p>
+                  <p>{boxData.Ville}</p>
+                  <p>{boxData.Pays}</p>
+                  <p>{boxData.Remarque}</p>
+                </Popup>
+              </Marker>
+            );
+          })}
+      </div>
     </MapContainer>
   );
 };
