ea50a2215bcdbc579d802d8ee0c75d3fab8a9ccd
Morgane Broutet <morgane.broutet@gmail.com>
2021-01-19 15:19:50 +0100

    add marker clusters & new marker icon

--- src/components/BoxesMap/BoxesMap.js
+++ src/components/BoxesMap/BoxesMap.js
@@ -1,27 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { readRemoteFile } from "react-papaparse";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from "leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 import "leaflet/dist/leaflet.css";
+import "react-leaflet-markercluster/dist/styles.min.css";
 import "./BoxesMap.css";
 
-import icon from "leaflet/dist/images/marker-icon.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
-
 const DefaultIcon = L.icon({
-  iconUrl: icon,
-  shadowUrl: iconShadow,
+  iconUrl: "./img/library.svg",
+  iconSize: [32, 32],
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const BoxesMap = () => {
   const [boxesData, setBoxesData] = useState([]);
-  const [hasData, setHasData] = useState(false);
+  const [error, setError] = useState(null);
   const position = [48.856614, 2.3522219];
 
-  if (!hasData) {
-    setHasData(true);
+  useEffect(() => {
     readRemoteFile("./data/boite_a_lire.csv", {
       header: true,
       comments: "//",
@@ -29,45 +27,53 @@ const BoxesMap = () => {
       complete: (results) => {
         setBoxesData(results.data);
       },
-      error: (error) => {
-        console.error("error:", error);
+      error: (err) => {
+        setError(err);
+      },
+      transform: (value, col) => {
+        if (col === "Coord_GPS") {
+          let res = value.split(",");
+          res = res.map((i) => Number(i));
+          return res;
+        }
+        return value;
       },
       skipEmptyLines: "greedy",
     });
-  }
+  }, []);
 
   return (
-    <MapContainer
-      className="map-container"
-      center={position}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <div>
-        {boxesData.length > 0 &&
-          boxesData.map((boxData, index) => {
-            const boxPositionGPS = boxData.Coord_GPS;
-            const boxPositionStr = boxPositionGPS.split(",");
-            const boxPosition = boxPositionStr.map((i) => Number(i));
-            return (
-              // eslint-disable-next-line react/no-array-index-key
-              <Marker key={index} position={boxPosition}>
-                <Popup>
-                  <p>{boxData.Adresse}</p>
-                  <p>{boxData.Code_Postal}</p>
-                  <p>{boxData.Ville}</p>
-                  <p>{boxData.Pays}</p>
-                  <p>{boxData.Remarque}</p>
-                </Popup>
-              </Marker>
-            );
-          })}
-      </div>
-    </MapContainer>
+    <>
+      {error && <p>{error}</p>}
+      <MapContainer
+        className="map-container"
+        center={position}
+        zoom={6}
+        scrollWheelZoom={false}
+        preferCanvas={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <MarkerClusterGroup showCoverageOnHover={false}>
+          <>
+            {boxesData.length > 0 &&
+              boxesData.map((boxData, index) => (
+                <Marker key={index} position={boxData.Coord_GPS}>
+                  <Popup>
+                    <p>{boxData.Adresse}</p>
+                    <p>{boxData.Code_Postal}</p>
+                    <p>{boxData.Ville}</p>
+                    <p>{boxData.Pays}</p>
+                    <p>{boxData.Remarque}</p>
+                  </Popup>
+                </Marker>
+              ))}
+          </>
+        </MarkerClusterGroup>
+      </MapContainer>
+    </>
   );
 };
 
